1e6c43db3ed6396eb9d86a445142280ecc58e9e4
Alexander Frolov <thephilosoft@gmail.com>
2016-12-25 18:12:05 +0300

    shame on me

--- application/properties/handlers/json/views/frontend-edit.php
+++ application/properties/handlers/json/views/frontend-edit.php
@@ -1,2 +1,1 @@
-<?php die("frontend-edit"); ?>
-<?= $form->field($model, $attribute_name)->widget(\devgroup\jsoneditor\Jsoneditor::className()) ?>
+<?= $form->field($model, $attribute_name)->widget(\devgroup\jsoneditor\Jsoneditor::class) ?>
